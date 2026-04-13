5d7cd50c11ab04de8371b9b6bd8e4889e2d04919
Luis Nunes <luisraj97@gmail.com>
2026-04-13 20:17:16 -0300

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,5 +1,1 @@
-// Electron Preload Script
-// O frontend se comunica diretamente com o backend Rust via fetch (localhost:5181)
-// Se precisarmos de comunicação IPC no futuro, adicionaremos aqui.
-
 console.log("ChestFlow Electron Preload Loaded");
